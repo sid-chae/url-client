9ba9f79fd7d2cdaead018a25ac7fb479c16a2bab
Sidney Chae <sidney_y_chae@homedepot.com>
2019-10-17 17:43:12 -0400

    prep for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src','server'),
+    entry: path.resolve(__dirname, 'src','app'),
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
